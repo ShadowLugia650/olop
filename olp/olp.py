98042e25587bcf04c88999e104c07e05d6db75c8
m-ks-woo <61434583+m-ks-woo@users.noreply.github.com>
2022-01-07 02:10:07 -0600

    inheritance update

--- olp/olp.py
+++ olp/olp.py
@@ -10,4 +10,4 @@
 #       access to variables modified within the loop
 #  primitives["dowhile"](scopevar: dict, cond: str, do: str) -> dict:
 #    A do-while equivalent to the while loop, with the same params and return value
-primitives = {"while": lambda scopevar, cond, do: globals().update(scopevar) or globals().update(wl = [1]) or any([wl.append(1) or exec(do) for i in wl if eval(cond) and len(wl) < 1000000]) or globals(), "dowhile": lambda scopevar, cond, do: globals().update(scopevar) or globals().update(wl = [1]) or (exec(do) and False) or any([wl.append(1) or exec(do) for i in wl if eval(cond) and len(wl) < 1000000]) or globals(),}
+primitives = {"while": lambda scopevar, cond, do: globals().update(scopevar) or globals().update(wl = [1]) or any([wl.append(1) or exec(do) for i in wl if eval(cond) and len(wl) < 1000000]) or globals(), "dowhile": lambda scopevar, cond, do: globals().update(scopevar) or globals().update(wl = [1]) or (exec(do) and False) or any([wl.append(1) or exec(do) for i in wl if eval(cond) and len(wl) < 1000000]) or globals(), "inherit": lambda class_: class_.update(class_["superclass"]) if "superclass" in class_ else False}
